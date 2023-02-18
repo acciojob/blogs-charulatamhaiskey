542d676fd08453e725dc8d75cfa9fb54c033aeb7
Charulata Mhaiskey <charu.mhaiskey96@gmail.com>
2023-02-18 22:48:44 +0530

    commitfirst

--- src/main/java/com/driver/services/UserService.java
+++ src/main/java/com/driver/services/UserService.java
@@ -1,7 +1,7 @@
 package com.driver.services;
 
 import com.driver.models.*;
-import com.driver.repositories.UserRepository;
+import com.driver.repositories.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -14,15 +14,23 @@ public class UserService {
     UserRepository userRepository3;
 
     public User createUser(String username, String password){
-
-
+        User user = new User(username, password);
+        userRepository3.save(user);
+        return user;
     }
 
     public void deleteUser(int userId){
-
+        userRepository3.deleteById(userId);
     }
 
-    public User updateUser(Integer id, String password){
-
+    public User updateUser(Integer id, String password)  {
+        User user;
+//        if(!userRepository3.findById(id).isPresent()) {
+//           throw new Exception();
+//        }
+        user = userRepository3.findById(id).get();
+        user.setPassword(password);
+        userRepository3.save(user);
+        return user;
     }
 }
